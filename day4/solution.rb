6892251fe0daeb88f07551b931a23eb6a84bc714
Chris Gannon <cjgannon@gmail.com>
2022-12-06 12:36:51 +0000

    update day 4 solution

--- day4/solution.rb
+++ day4/solution.rb
@@ -36,14 +36,14 @@ def overlap(first_group, second_group)
 
   return true if fg[0].to_i <= sg[0].to_i && fg[1].to_i >= sg[1].to_i
 
-  return true if fg[0].to_i >= sg[0].to_i && fg[1].to_i <= sg[1].to_i
+  fg[0].to_i >= sg[0].to_i && fg[1].to_i <= sg[1].to_i
 end
 
 def partial_overlap(first_group, second_group)
   fg = first_group.split("-")
   sg = second_group.split("-")
 
-  return false if (fg[0].to_i > sg[1].to_i || fg[1].to_i < sg[0].to_i)
+  !(fg[0].to_i > sg[1].to_i || fg[1].to_i < sg[0].to_i)
 end
 
 puts "There are #{overlapping_pairs} overlapping groups"
